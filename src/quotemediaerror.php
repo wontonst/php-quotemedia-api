0c5b7715498c9a127c6cb1f85852d843289a58ff
Roy Zheng <roycraft3@gmail.com>
2014-10-27 18:01:45 -0700

    bugfix

--- src/quotemediaerror.php
+++ src/quotemediaerror.php
@@ -16,9 +16,9 @@ final class QuoteMediaError {
             QuoteMediaError::GOOD => 'No error has occurred.',
             QuoteMediaError::API_HTTP_REQUEST_ERROR => 'Could not access API due to HTTP error',
             QuoteMediaError::INPUT_IS_NOT_ARRAY => 'Expected an array on last function call',
-            QuoteMediaError::GET_QUOTES_EXCEED_MAX_SYMBOL => 'getQuotes cannot request more than ' . QuoteMediaStocks::GET_QUOTES_MAX_SYMBOLS . ' symbols at a time.',
-            QuoteMediaError::GET_PROFILES_EXCEED_MAX_SYMBOL => 'getProfiles cannot request more than ' . QuoteMediaStocks::GET_PROFILES_MAX_SYMBOLS . ' symbols at a time.',
-            QuoteMediaError::GET_FUNDAMENTALS_EXCEED_MAX_SYMBOL => 'getFundamentals cannot request more than ' . QuoteMediaStocks::GET_FUNDAMENTALS_MAX_SYMBOLS . ' symbols at a time.',
+            QuoteMediaError::GET_QUOTES_EXCEED_MAX_SYMBOL => 'getQuotes cannot request more than ' . QuoteMediaConst::GET_QUOTES_MAX_SYMBOLS . ' symbols at a time.',
+            QuoteMediaError::GET_PROFILES_EXCEED_MAX_SYMBOL => 'getProfiles cannot request more than ' . QuoteMediaConst::GET_PROFILES_MAX_SYMBOLS . ' symbols at a time.',
+            QuoteMediaError::GET_FUNDAMENTALS_EXCEED_MAX_SYMBOL => 'getFundamentals cannot request more than ' . QuoteMediaConst::GET_FUNDAMENTALS_MAX_SYMBOLS . ' symbols at a time.',
         );
     }
 
