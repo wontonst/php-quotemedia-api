623e3da102c7cf25d37b36d61ac5e44c12b7a373
Roy Zheng <roycraft3@gmail.com>
2014-10-27 17:42:11 -0700

    test files

--- tests/QuoteMediaStocksTest.php
+++ tests/QuoteMediaStocksTest.php
@@ -19,27 +19,10 @@ class QuoteMediaStocksTest extends PHPUnit_Framework_TestCase {
                 $this->assertInternalType('array', $row, 'A row in ' . $v . ' is type ' . gettype($row) . ' instead of array. Dump: ' . print_r($row, true));
             }
         }
-    }
-
-    public function testGetAssoc() {
-        $functions = array('getProfiles', 'getQuotes', 'getFundamentals');
-
-        $arr = array('AAPL', 'GOOG', 'LUV');
-
-        foreach ($functions as $v) {
-            $result = $this->api->$v($arr, true);
-            $this->assertInternalType('array', $result, 'Error is ' . QuoteMediaError::IDtoError($this->api->getErrorID()));
-            $this->assertEquals(count($arr), count($result), 'Array size returned by ' . $v . ' does not matched input array size.');
-
-            foreach ($result as &$row) {
-                $this->assertInternalType('array', $row, 'A row in ' . $v . ' is type ' . gettype($row) . ' instead of array. Dump: ' . print_r($row, true));
-            }
-            foreach ($arr as $ticker) {
-                $this->assertTrue(isset($result[$ticker]));
-            }
+        foreach($result as $v){
+            
         }
     }
-
 }
 
 ?>
