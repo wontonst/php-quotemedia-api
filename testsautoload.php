0b0ff403287295dad2273f2c845fa678df242cbb
Roy Zheng <roycraft3@gmail.com>
2014-11-07 15:35:47 -0800

    tests loadig

--- testsautoload.php
+++ testsautoload.php
@@ -2,6 +2,14 @@
 
 include(__DIR__.'/autoload.php');
 
-define('TEST_WEBMASTER_ID', 0);//ENTER YOUR WEBMASTER ID HERE. DONT FORGET TO REMOVE IT WHEN YOU'RE COMMITING
-//use git update-index --assume-unchanged testsautoload.php
+/*
+ENTER YOUR WEBMASTER ID HERE. DONT FORGET TO REMOVE IT WHEN YOU'RE COMMITING
+Easy local repo cmd so you don't accidentally push your webmaster id up: git update-index --assume-unchanged testsautoload.php
+*/
+define('TEST_WEBMASTER_ID', );//WEBMASTER ID HERE
+
+spl_autoload_register(function($class){
+    @include_once(__DIR__.'/tests/'.$class.'.php');
+  });
+
 ?>
