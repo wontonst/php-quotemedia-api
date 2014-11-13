3596e8d1693aa043da440295b90b680c4321be97
Roy Zheng <roycraft3@gmail.com>
2014-11-12 18:19:53 -0800

    test runner change

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-phpunit -d display_errors --tap --coverage-text=coverage.txt --stop-on-failure --stop-on-error --bootstrap testsautoload.php tests
+phpunit -d display_errors --tap --coverage-html test_coverage --stop-on-failure --stop-on-error --bootstrap testsautoload.php tests
