0e0a142aa7209a45157b495eb163166b4b460de5
Roy Zheng <roycraft3@gmail.com>
2014-11-26 09:03:04 -0800

    result class added

--- src/quotemediaresult.php
+++ src/quotemediaresult.php
@@ -1,12 +1,15 @@
 <?php
 
-class QuoteMediaResult {
+/**
+ * Class that stores output of a call to the library. Contains error information and call result. This class should not be instantiated by the user.
+ */
+abstract class QuoteMediaResult {
 
     protected $error;
     protected $result;
 
-    public function __construct($result) {
-        $this->error = QuoteMediaError::GOOD;
+    public function __construct($result, $error) {
+        $this->error = $error;
     }
 
     public function getErrorID() {
@@ -15,43 +18,6 @@ class QuoteMediaResult {
 
     public function getError() {
         return QuoteMediaError::IDtoError($this->error);
-    }
-
-    /**
-     * Check that the input array of symbols is well formed.
-     * @param array $input array of tickers, presumably
-     * @return boolean whether or not the array is well formed
-     */
-    protected function verifySymbolArray(&$input) {
-        if (!is_array($input)) {
-            $this->error = QuoteMediaError::INPUT_IS_NOT_ARRAY;
-            return false;
-        }
-        foreach ($input as $v) {
-            if (!is_string($v)) {
-                $this->error = QuoteMediaError::SYMBOL_IS_NOT_STRING;
-                return false;
-            }
-            if (0 == preg_match('/^[a-zA-Z\-.]{1,10}$/', trim($v))) {
-                $this->error = QuoteMediaError::MALFORMED_SYMBOL;
-                $this->error_info = trim($v);
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * QuoteMedia doesn't like tickers like"BRK-A" or CRD.B, instead it wants BRKA and CRDB. This function performs the transformation.
-     * @param array $input array of tickers
-     * @return array array of cleaned tickers
-     */
-    protected function cleanSymbolArray(&$input) {
-        $res = array();
-        foreach ($input as $symbol) {
-            $res[] = str_replace('-', '', str_replace('.', '', $symbol));
-        }
-        return $res;
     }
 
 }
---
+++ src/quotemediastocksresult.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Class that stores the result of a call to the stocks library. This class should not be instantiated by the user.
+ */
+class QuoteMediaStocksResult {
+
+    protected $has_missing; /// <boolean of whether or not there are missing symbols
+    protected $missing; ///< array of missing symbols
+
+    /**
+     * Construct an object to store results of a call to stocks library.
+     * @param array $result result of the call
+     * @param array $missing list of missing symbols
+     */
+
+    public function __construct($result, $missing = NULL) {
+        if ($missing != NULL) {
+            $this->$has_missing = true;
+            $this->missing = $missing;
+            parent::construct($result, QuoteMediaError::SYMBOL_DOES_NOT_EXIST);
+        } else {
+            $this->$has_missing = false;
+            $this->missing = array();
+            parent::construct($result, QuoteMediaError::GOOD);
+        }
+    }
+
+    /**
+     * Whether or not symbols are missing.
+     * @return bool
+     */
+    public function hasMissingSymbols() {
+        return $this->$has_missing;
+    }
+
+    /**
+     * Return list of missing symbols.
+     * @return array missing symbols
+     */
+    public function getMissingSymbols() {
+        return $this->missing;
+    }
+
+}
+
+?>
