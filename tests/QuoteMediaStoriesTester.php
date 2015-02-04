f5252eeceb66d20d0f9331d77dabedc71bdc9a7f
Roy Zheng <roycraft3@gmail.com>
2015-02-04 00:05:42 -0800

    fiX

--- tests/QuoteMediaStoriesTester.php
+++ tests/QuoteMediaStoriesTester.php
@@ -3,15 +3,15 @@
 abstract class QuoteMediaStoriesTester extends PHPUnit_Framework_TestCase {
 
     protected function setUpInputs() {
-       $this->stdTopics = array(
-           'GOOG','AAPL',
-       );
+        $this->stdTopics = array(
+            'GOOG', 'AAPL',
+        );
     }
 
     protected function verifyHeadlines($result, $expected_error, $expected_tsize, $expected_nsize) {
-        $this->assertEquals($result->getErrorID(), $expected_error);
-        $this->assertEquals($result->getTopicSize(), $expected_tsize);
-        $this->assertEquals($result->getNewsSize(), $expected_nsize);
+        $this->assertEquals($expected_error, $result->getErrorID());
+        $this->assertEquals($expected_tsize, $result->getTopicSize());
+        $this->assertEquals($expected_nsize, $result->getNewsSize());
         $this->assertEquals(count($result->getResult()), $expected_nsize);
         foreach ($result->getResult() as $r) {
             $expected_keys = array('newsid', 'datetime', 'source', 'headline', 'storyurl');
@@ -21,6 +21,5 @@ abstract class QuoteMediaStoriesTester extends PHPUnit_Framework_TestCase {
             }
         }
     }
-
 
 }
